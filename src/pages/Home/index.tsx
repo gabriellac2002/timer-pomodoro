888e2727bd7453c7ed7fe4a0fbde9389c0a79aa1
gabriellac2002 <gabriellacsilva2002@gmail.com>
2025-01-17 14:25:55 -0300

    refactor Home page: enhance layout with styled components and improve form structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,16 @@
 import { Play } from "phosphor-react";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from "./styles";
 
 export function Home() {
   return (
-    <div>
+    <HomeContainer>
       <form>
-        <div>
+        <FormContainer>
           <label htmlFor="task">Vou trabalhar em:</label>
           <input id="task" type="text" />
 
@@ -12,20 +18,20 @@ export function Home() {
           <input id="minutesAmount" type="number" />
 
           <span>minutos.</span>
-        </div>
+        </FormContainer>
 
-        <div>
+        <CountdownContainer>
           <span>0</span>
           <span>0</span>
-          <span>:</span>
+          <Separator>:</Separator>
           <span>0</span>
           <span>0</span>
-        </div>
+        </CountdownContainer>
 
         <button type="submit">
           <Play size={24} /> Começar
         </button>
       </form>
-    </div>
+    </HomeContainer>
   );
 }
